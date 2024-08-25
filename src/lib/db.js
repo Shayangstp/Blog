9a33887f6d428b412e3b50e46a406e0540d8dcc3
Shayan Golestanipour <g.shayan5529@gmail.com>
2024-08-25 18:32:15 +0330

    add mongo atlas url

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 
 // const url = process.env.MONGODB_URI!;
 
-// const url =
-//   "mongodb+srv://shayanGstp:Shayan5262@shayangstp.lk8wplv.mongodb.net/next_ecom?retryWrites=true&w=majority&appName=ShayanGstp";
+const url =
+  "mongodb+srv://shayanGstp:Shayan5262@shayangstp.lk8wplv.mongodb.net/blog?retryWrites=true&w=majority&appName=ShayanGstp";
 
 const startDb = async () => {
-  console.log("Checking TextEncoder:", typeof TextEncoder);
-  const url = "mongodb://127.0.0.1:27017/blog";
+
   let connection = "";
   try {
     if (!connection) {
