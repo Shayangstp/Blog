5199486d2cfde8c9f061052c14d46971ff317de5
Shayan Golestanipour <g.shayan5529@gmail.com>
2024-08-25 18:59:40 +0330

    add mongo url

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 // const url = process.env.MONGODB_URI!;
 
+// const url =
+//   "mongodb+srv://shayanGstp:Shayan5262@shayangstp.lk8wplv.mongodb.net/blog?retryWrites=true&w=majority&appName=ShayanGstp";
+
 const url =
-  "mongodb+srv://shayanGstp:Shayan5262@shayangstp.lk8wplv.mongodb.net/blog?retryWrites=true&w=majority&appName=ShayanGstp";
+  "mongodb+srv://vercel-admin-user-66cb4c4f74938630adc6439b:GZzrYMhuVp7C11M4@shayangstp.lk8wplv.mongodb.net/blog?retryWrites=true&w=majority";
 
 const startDb = async () => {
 
